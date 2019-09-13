f15eb6c6b32c3d678fe8c68ba68ea1f30e4e6ea0
Houria <houria.danesh@gmail.com>
2019-09-13 09:38:49 +0200

    Add files via upload

---
+++ javascript/javascript1/week2/CandyHelper.js
@@ -0,0 +1,44 @@
+const boughtCandyPrices = [];
+
+function addCandy(candyType , weight )
+{
+    if (candyType === 'sweet') {
+        return boughtCandyPrices.push(0.5 * weight);
+    }
+    else if (candyType === 'chocolate') {
+        return boughtCandyPrices.push(0.7 * weight);
+    }
+    else if (candyType === 'toffe') {
+        return boughtCandyPrices.push(1.1 * weight);
+    }
+    else (candyType === 'chewing-gum')
+     {
+        return boughtCandyPrices.push(0.03 * weight);
+    }
+}
+
+addCandy("sweet", 20);
+console.log(`Total price: ${boughtCandyPrices} kr.`);
+
+// Can i buy more?
+
+const amountToSpend = Math.random() * 100;
+console.log(`You just have this amount to spend: ${amountToSpend}`);
+
+function canBuyMoreCandy()
+{    
+       let totalPrice = 0;
+    for (let i = 0; i < boughtCandyPrices.length ; i++)
+     {
+        totalPrice += boughtCandyPrices[i];
+    }
+    return totalPrice;
+}
+
+if(canBuyMoreCandy()) {
+    console.log("You can buy more, so please do!"); 
+   
+  }
+  else {
+    console.log("Enough candy for you!");
+  }
