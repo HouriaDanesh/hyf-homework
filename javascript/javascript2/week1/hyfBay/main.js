87cfff138cd0345095d5e08402852090964a9375
Houria <houria.danesh@gmail.com>
2019-10-04 11:35:46 +0200

    Add files via upload

--- javascript/javascript2/week1/hyfBay/main.js
+++ javascript/javascript2/week1/hyfBay/main.js
@@ -1,3 +1,42 @@
 console.log('Script loaded');
 
 console.log(getAvailableProducts());
+
+const testProductNames = [
+    'Flat screen',
+    'Mobile phone',
+     'Wallet'];
+
+renderProducts(testProductNames); // Should add 3 li's to the ul under the products section with Flat screen, Mobile phone, Wallet text
+
+let parent = document.querySelector("ul");
+
+function createList(parent, testProductNames)
+ {
+    array.forEach(function () 
+    {
+        const li = document.createElement("li");
+        li.textContent = testProductNames;
+        parent.appendChild(li);
+
+    })
+}
+
+
+const products = getAvailableProducts();
+console.log(products) // logs out
+/*
+[{
+    id: 23771823,
+    name: 'Flat screen',
+    price: 4000,
+    rating: 4.2,
+    shipsTo: [ 'denmark', 'germany'],
+},
+...]
+*/
+
+
+
+
+
