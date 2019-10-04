89a6836fc5d7ed990de1b57c98a1e7830a6ae734
Houria <houria.danesh@gmail.com>
2019-10-04 12:04:55 +0200

    Add files via upload

--- javascript/javascript2/week1/hyfBay/main.js
+++ javascript/javascript2/week1/hyfBay/main.js
@@ -1,42 +1,50 @@
-console.log('Script loaded');
+//console.log("Script loaded");
+//const testProductNames = ["Flat screen", "Mobile phone", "Wallet"];
 
-console.log(getAvailableProducts());
+function getAvailableProducts() {
+    return [{
+        id: 23771823,
+        name: 'Flat screen',
+        price: 4000,
+        rating: 4.2,
+        shipsTo: ['Denmark', 'Germany']
+       },
+       {
+        id: 23771824,
+        name: 'Mobile Phones',
+        price: 14000,
+        rating: 4.6,
+        shipsTo: ['Germany','Sweden','Norway','Denmark']
+       },
+       {
+        id: 23771825,
+        name: 'Wallets',
+        price: 4000,
+        rating: 4.2,
+        shipsTo: ['Denmark']
+       }]
+  }
+  const products = getAvailableProducts();
+  console.log(products); // logs out
+  /*
+  [{
+      id: 23771823,
+      name: 'Flat screen',
+      price: 4000,
+      rating: 4.2,
+      shipsTo: [ 'denmark', 'germany'],
+  },
+  ...]
+  */
+  
+//renderProducts(testProductNames); // Should add 3 li's to the ul under the products section with Flat screen, Mobile phone, Wallet text
 
-const testProductNames = [
-    'Flat screen',
-    'Mobile phone',
-     'Wallet'];
-
-renderProducts(testProductNames); // Should add 3 li's to the ul under the products section with Flat screen, Mobile phone, Wallet text
-
-let parent = document.querySelector("ul");
-
-function renderProducts(parent, testProductNames)
- {
-    array.forEach(function () 
-    {
-        const li = document.createElement("li");
-        li.textContent = testProductNames;
-        parent.appendChild(li);
-
-    })
+const parent = document.querySelector("section.products > ul");
+function renderProducts(products) {
+products.forEach(function() {
+    const li = document.createElement("li");
+    li.textContent = testProductNames;
+    parent.appendChild(li);
+ });
 }
-
-
-const products = getAvailableProducts();
-console.log(products) // logs out
-/*
-[{
-    id: 23771823,
-    name: 'Flat screen',
-    price: 4000,
-    rating: 4.2,
-    shipsTo: [ 'denmark', 'germany'],
-},
-...]
-*/
-
-
-
-
 
