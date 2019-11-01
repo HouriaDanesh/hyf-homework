da5e3aab35fba09d30215c87b6b61508e0403b27
Houria <houria.danesh@gmail.com>
2019-11-01 12:03:45 +0100

    Update main.js

--- javascript/javascript3/week2/promise-visual/main.js
+++ javascript/javascript3/week2/promise-visual/main.js
@@ -19,19 +19,19 @@ moveElement(document.querySelector('ul.marks li:nth-child(1)'), {x: 20, y: 300})
 
 translateOneByOne();
 
-     function translateAllAtOnce()
-     {
-         Promise.all([
-         moveElement(document.querySelector('ul.marks li:nth-child(1)'), {x: 20, y: 300}),
-         moveElement(document.querySelector('ul.marks li:nth-child(2)'), {x: 400, y: 300}),
-         moveElement(document.querySelector('ul.marks li:nth-child(3)'), {x: 400, y: 20})])
+    // function translateAllAtOnce()
+    // {
+//          Promise.all([
+//          moveElement(document.querySelector('ul.marks li:nth-child(1)'), {x: 20, y: 300}),
+//          moveElement(document.querySelector('ul.marks li:nth-child(2)'), {x: 400, y: 300}),
+//          moveElement(document.querySelector('ul.marks li:nth-child(3)'), {x: 400, y: 20})])
        
-         .then(() => {
-             console.log('All circles have been moved')
-         });
+//          .then(() => {
+//              console.log('All circles have been moved')
+//          });
     
 
-     } 
+//      } 
 
-     translateAllAtOnce();
+//      translateAllAtOnce();
      
